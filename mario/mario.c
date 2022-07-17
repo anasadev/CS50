d4aa36fce6401cca04816584a021fec13a69f617
Ana Sá <anasadevweb@gmail.com>
2022-07-17 15:58:42 +0200

    Final version

--- mario/mario.c
+++ mario/mario.c
@@ -2,15 +2,23 @@
 #include <cs50.h>
 
 void brick(void);
+void espace(void);
+int get_positive_int(void);
 
 
 int main(void)
 {
-    int height = 5;
+    //Ask user the pyramid height (number between 1 and 8)
+    int height = get_positive_int();
 
-    for (int counter = 0; counter < height; counter++)//quantidade de linhas
+    //number of rows
+    for (int counter = 0; counter < height; counter++)
     {
-        for (int j = 0; j < counter+1; j++)//quantidade de # em cada linha
+        for (int i = 0; i < height - counter - 1; i++){
+            espace();
+        }
+        //number of bricks in wich row
+        for (int j = 0; j < counter+1; j++)
         {
             brick();
 
@@ -19,6 +27,22 @@ int main(void)
     }
 }
 
+int get_positive_int(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Choose a number from 1 to 8 to the height of your pyramid\n");
+    }
+    while(n < 1 || n > 8);
+    return n;
+}
+
+
 void brick(void){
     printf("#");
+}
+
+void espace(void){
+    printf(" ");
 }
