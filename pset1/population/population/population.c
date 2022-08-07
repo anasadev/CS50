dee7fa7936986e998a3c647c7f88e6a6300f3532
Ana Sá <anasadevweb@gmail.com>
2022-08-07 19:35:22 +0200

    Add population exercise

---
+++ pset1/population/population/population.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int final;
+int years;
+int pop;
+
+int main(void)
+{
+      //Ask user the initial population
+      do
+      {
+         pop = get_int("Initial population:");
+      }
+      while (pop < 9);
+
+     //Ask user the final population
+     do
+      {
+         final = get_int("Final population:");
+      }
+      while (final < pop);
+
+     //Calculate how many years to reach final population
+     for (years = 0; pop < final; years++){
+         pop = pop + pop / 3 - pop / 4;
+     }
+
+     printf("Years: %i\n", years);
+
+}
